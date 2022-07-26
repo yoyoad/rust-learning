50abaa873baa3b96bab672f7faa07b35a6984013
yoyoad <yoyoad.adi@gmail.com>
2022-07-27 01:25:39 +0300

    Added command line parameters to fibbonaci script :)

--- src/fibbonaci.rs
+++ src/fibbonaci.rs
@@ -1,7 +1,7 @@
-pub fn nth_fibbonaci(n: u32)-> u32 {
+pub fn nth_fibbonaci(n: &u128)-> u128 {
     let (mut n1, mut n2) = (0, 1);
-    let mut current_value: u32 = 0;
-    for _ in 1..n {
+    let mut current_value: u128 = 0;
+    for _ in 1..*n {
         current_value = n1 + n2;
         n1 = n2;
         n2 = current_value;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::env;
 mod fibbonaci;
 
 fn main() {
-    let n = 10;
-    let fb = fibbonaci::nth_fibbonaci(n);
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}",args);
+    let n = if args.len() >= 2 {
+        args[1].to_string().parse::<u128>().unwrap()
+    } else {
+        println!("{}",args.len());
+        0
+    };
+
+    let fb = fibbonaci::nth_fibbonaci(&n);
     println!("{n}th fibbonaci - {fb}");
 }
